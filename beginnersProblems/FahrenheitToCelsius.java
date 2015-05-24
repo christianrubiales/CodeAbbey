30ac2f3957e3e1cdf228fff4131fb72c5c14f6f1
Christian Rubiales <christianrubiales@yahoo.com>
2015-05-24 21:58:22 +0800

    FahrenheitToCelsius

---
+++ beginnersProblems/FahrenheitToCelsius.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class FahrenheitToCelsius {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print(Math.round((s.nextDouble() - 32) * 5 / 9) + " ");
+		}
+	}
+
+}
